2172d70029acf748dd704716a94fa92e0bde1d98
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-07-06 18:12:34 +0200

    fixed error on edit.blade

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -20,21 +20,41 @@
 
                     <label for="name">title</label>
                     <input class="form-control" type="text" name="title" value="{{ $comic->title }}">
+                    @error('title')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
 
                     <label for="name">description</label>
                     <input class="form-control" type="text" name="description" value="{{ $comic->description }}">
+                    @error('description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+
 
                     <label for="name">thumb</label>
                     <input class="form-control" type="text" name="thumb" value="{{ $comic->thumb }}">
+                    @error('thumb')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
 
                     <label for="name">price</label>
                     <input class="form-control" type="text" name="price" value="{{ $comic->price }}">
+                    @error('price')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+
 
                     <label for="name">series</label>
                     <input class="form-control" type="text" name="series" value="{{ $comic->series }}">
+                    @error('series')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
 
                     <label for="name">sale date</label>
                     <input class="form-control" type="text" name="sale date" value="{{ $comic->sale_date }}">
+                    @error('sale_date')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
 
                     <label for="name">type</label>
                     <select name="type" id="">
@@ -46,14 +66,12 @@
 
                     <input class="form-control mt-4 btn btn-primary" type="submit" value="Invia">
 
-                    <form id="form" action="{{ route('comics.destroy', $comic) }}">
-                        @csrf
-                        @method('DELETE')
-                        <input onclick="confirmDelete()" class="form-control mt-4 btn btn-danger" type="submit"
-                            value="Cancel">
 
-                    </form>
-
+                </form>
+                <form id="form" action="{{ route('comics.destroy', $comic) }}">
+                    @csrf
+                    @method('DELETE')
+                    <input onclick="confirmDelete()" class="form-control mt-4 btn btn-danger" type="submit" value="Cancel">
                 </form>
             </div>
         </div>
