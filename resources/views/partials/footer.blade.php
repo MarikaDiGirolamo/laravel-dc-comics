de1759cd058e7bac65b76a5c2847efd08aff13c7
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-07-06 10:08:22 +0200

    Update header and footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,62 @@
-<footer class="p-3">
+<footer>
 
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit.</footer>
+    @php
+        $links = config('store.links');
+        $footerCols = config('store.footerCols');
+        $footerSocialMedias = config('store.footerSocialMedias');
+    @endphp
+
+
+    <div id="footer-top-section">
+        <div class="container">
+            <div class="row justify-content-start">
+                <div class="col-lg-5">
+                    <div class="row h-100 py-5">
+                        @foreach ($footerCols as $col)
+                            <div class="col-4 d-flex justify-content-start flex-column">
+                                @foreach ($col as $topic)
+                                    <div class="links-list text-white">
+                                        <h5>{{ $topic['heading'] }}</h5>
+                                        <ul class="text-white p-0">
+                                            @foreach ($topic['links'] as $link)
+                                                <a href="#">
+                                                    <li>{{ $link }}</li>
+                                                </a>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endforeach
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div id="footer-bottom-section" class="_flex-center">
+        <div class="container">
+            <div class="row d-flex justify-content-between align-items-center">
+                <div id="sign-up" class="col-4 d-flex justify-content-center align-items-center text-white">
+                    <a href="#">
+                        <h5 class="p-1 text-white m-0">SIGN-UP NOW!</h5>
+                    </a>
+                </div>
+                <div id="social-media" class="col-4 _flex-center gap-3">
+                    <div>
+                        <a href="#">
+                            <h5 class="p-1 m-0">FOLLOW US</h5>
+                        </a>
+                    </div>
+                    <div class="text-secondary d-none d-lg-flex gap-2 fs-5">
+                        @foreach ($footerSocialMedias as $icon)
+                            <a href="#"><img class="img-fluid"
+                                    src="{{ Vite::asset('/resources/img/' . $icon['path']) }}"
+                                    :alt="{{ $icon['media'] }}"></a>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,29 @@
-<header class="p-3">
+@php
+    $links = config('store.links');
+    $footerCols = config('store.footerCols');
+    $footerSocialMedias = config('store.footerSocialMedias');
+@endphp
 
+<header class="bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-5 d-flex align-items-center">
+                <a href="#"><img id="header-logo" class="img-fluid"
+                        src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC Logo"></a>
+            </div>
+            <div class="col-7 d-flex justify-content-end align-items-center">
+                <ul class="d-none d-lg-flex m-0">
+                    @foreach ($links as $link)
+                        <li class="d-flex flex-column">
+                            <a href="#">{{ $link }}</a>
+                            <div></div>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="d-lg-none _flex-center fs-1">
+                    <i class="fa-solid fa-bars"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
