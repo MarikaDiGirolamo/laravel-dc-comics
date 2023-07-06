1acdc42624721a6a8f402b2451f9bf965e184f60
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-07-06 10:08:09 +0200

    Update store and scss

--- config/store.php
+++ config/store.php
@@ -1,6 +1,64 @@
 <?php
 
 return [
+    "links" => [
+        'Characters',
+        'Comics',
+        'Movies',
+        'Tv',
+        'Games',
+        'Collectible',
+        'Videos',
+        'Fans',
+        'News',
+        'Shop'
+    ],
+    "footerCols" => [
+        [
+            [
+                "heading" => 'Dc Comics',
+                "links" => ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+            ],
+            [
+                "heading" => 'Shop',
+                "links" => ['Shop DC', 'Shop DC Collectibles']
+            ]
+        ],
+        [
+            [
+                "heading" => 'Dc',
+                "links" => ['Terms Of Use', 'Privacy Policy (New)', 'Ad Choices', 'Advertising', 'Jobs', 'Subscriptions', 'Talent Workshops', ' CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us']
+            ]
+        ],
+        [
+            [
+                "heading" => 'Sites',
+                "links" => ['DC', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa']
+            ]
+        ]
+    ],
+    "footerSocialMedias" => [
+        [
+            "media" => 'Facebook',
+            "path" => 'footer-facebook.png'
+        ],
+        [
+            "media" => 'Twitter',
+            "path" => 'footer-twitter.png'
+        ],
+        [
+            "media" => 'Youtube',
+            "path" => 'footer-youtube.png'
+        ],
+        [
+            "media" => 'Pinterest',
+            "path" => 'footer-pinterest.png'
+        ],
+        [
+            "media" => 'Periscope',
+            "path" => 'footer-periscope.png'
+        ]
+    ],
     'someLinks' => [
         [
             "href" => "/",
@@ -15,5 +73,5 @@ return [
             "text" => "Documentation",
         ]
     ],
-    'someOtherData' => [ 42, 37, 73],
+    'someOtherData' => [42, 37, 73],
 ];
